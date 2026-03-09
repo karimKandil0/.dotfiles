f658ed65f002ab6f2a6e31bcdad4bd6066c935e2
karimKandil0 <kimo989yt@gmail.com>
2026-03-09 19:41:58 +0200

    Removed networking.nix for simplicity

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,6 @@
   imports = [
     ./modules/system/globals.nix
     ./modules/system/hardware.nix
-    ./modules/system/networking.nix
     ./modules/system/packages.nix
     ./modules/system/services.nix
     ./modules/system/users.nix
